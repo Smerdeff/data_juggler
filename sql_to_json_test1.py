fb45c09439691e0533b7a5b4600de80fb507cc7f
Smerdeff <smerdeva@gmail.com>
2020-05-07 12:22:16 +0500

    Create sql_to_json_test1.py

---
+++ sql_to_json_test1.py
@@ -0,0 +1,49 @@
+"""
+CREATE PROC sp_data_juggler_test1 as
+select
+	'example' as [expamle],
+	GETDATE() as [date],
+	123.45 [Num],
+	null as [main:]
+
+select 'YT-12565' as DocNum,
+	1 [doc_id:],
+	convert(date,'04.05.2020') as DocDate,
+	null as [:main]
+union
+select 'MR-4545' as DocNum,
+	2 [doc_id:],
+	convert(date,'05.05.2020') as DocDate,
+	null as [:main]
+
+select 1 [:doc_id],
+	555666 [good_id],
+	12 as [qnt]
+union
+select 1 [:doc_id],
+	777888 [good_id],
+	6 as [qnt]
+union
+select 2 [:doc_id],
+	555666 [good_id],
+	2 as [qnt]
+union
+select 2 [:doc_id],
+	777888 [good_id],
+	7 as [qnt]
+
+"""
+
+from data_juggler import data_juggler
+
+if __name__ == '__main__':
+    source = "sqlserver://sa:pass@server/base/?"
+    data_source =source+ 'data=sp_data_juggler_test1'
+
+    dj = data_juggler(data_source)
+
+    # print(dj.data["data"])
+    dj.join("data")
+    # print(dj.data["data"][0])
+
+    print (dj.to_json("data"))
